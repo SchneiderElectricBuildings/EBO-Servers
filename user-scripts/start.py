64595a2116c8afc1e7bb2b4070a6ba842830701c
Samuel Smith <samuel.smith@se.com>
2024-11-08 06:41:12 +0000

    Updated documentation and scripts to HF-503-CP-13-14000-Consolidation

--- user-scripts/start.py
+++ user-scripts/start.py
@@ -2,7 +2,6 @@
 import argparse
 import subprocess
 import shlex
-import os
 
 def exe(cmd):
     try:
@@ -18,8 +17,6 @@ def get_arguments(description):
     parser.add_argument('--name', '-n', required=True, help='name of the container')
     parser.add_argument('--version', '-v', required=True, help='version of server to start')
     parser.add_argument('--ip', '-i', required=True, help='ip address of the container')
-    parser.add_argument('--graphdb', '-g', required=False, default=f'', help='the url to reach GraphDB')
-    parser.add_argument('--type', '-t', required=False, default='ebo-edge-server', help='type of server, defaults to ebo-edge-server, other values are: ebo-enterprise-server or ebo-enterprise-central')
     parser.add_argument('--accept-eula', required=True, help='''for the server to
      start you need to accept eula.
     To accept use: --accept-eula=Yes
@@ -28,12 +25,6 @@ def get_arguments(description):
         'containers get their ca certificates')
     parser.add_argument('--dns', default=None, help="optional dns server" \
         "address for when container can't reach host dns, because of, for example, VPN")
-    parser.add_argument('--http-proxy', default=None, help="optional http proxy " \
-        "if not given the host environment variables http_proxy or HTTP_PROXY will be used ")
-    parser.add_argument('--https-proxy', default=None, help="optional https proxy " \
-        "if not given the host environment variables https_proxy or HTTPS_PROXY will be used ")
-    parser.add_argument('--no-proxy', default=None, help="optional no proxy " \
-        "if not given the host environment variables no_proxy or NO_PROXY will be used ")
     return parser.parse_args()
 
 def run():
@@ -43,37 +34,10 @@ def run():
     ip = args.ip
     accept_eula = args.accept_eula
     ca_folder = args.ca_folder
-    graphdb = args.graphdb
-    server_type = args.type
     dns = args.dns
-    http_proxy=args.http_proxy
-    https_proxy=args.https_proxy
-    no_proxy=args.no_proxy
-    image = f'ghcr.io/schneiderelectricbuildings/{server_type}:{version}'
+    image = f'ghcr.io/schneiderelectricbuildings/ebo-edge-server:{version}'
     db_vol = f'{name}-db'
     db_folder = '/var/sbo'
-    proxy = ''
-    if http_proxy:
-        proxy += f'-e http_proxy={http_proxy} '
-    else:
-        if "http_proxy" in os.environ:
-            proxy += f'-e http_proxy={os.environ["http_proxy"]} '
-        if "HTTP_PROXY" in os.environ:
-            proxy += f'-e HTTP_PROXY={os.environ["HTTP_PROXY"]} '
-    if https_proxy:
-        proxy += f'-e https_proxy={https_proxy} '
-    else:
-        if "https_proxy" in os.environ:
-            proxy += f'-e https_proxy={os.environ["https_proxy"]} '
-        if "HTTPS_PROXY" in os.environ:
-            proxy += f'-e HTTPS_PROXY={os.environ["HTTPS_PROXY"]} '
-    if no_proxy:
-        proxy += f'-e no_proxy={no_proxy} '
-    else:
-        if "no_proxy" in os.environ:
-            proxy += f'-e no_proxy={os.environ["no_proxy"]} '
-        if "NO_PROXY" in os.environ:
-            proxy += f'-e NO_PROXY={os.environ["NO_PROXY"]} '
 
     cmd = f'docker run -d --name={name} -h {name} ' \
         '--ulimit core=-1 ' \
@@ -81,8 +45,6 @@ def run():
         '--network bridged-net ' \
         f'--mount type=bind,source=/var/crash,target=/var/crash ' \
         f'-e NSP_ACCEPT_EULA="{accept_eula}" ' \
-        f'-e Semantic_Db_URL="{graphdb}" ' \
-        f'{proxy}'\
         f'--ip {ip} ' \
         f'--mount source={db_vol},target={db_folder} '
     if ca_folder:
